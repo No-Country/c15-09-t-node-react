9185258f8321ff62db0021ec3aa8e5d0b4c5a286
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-11-28 18:42:24 -0300

    primera vista realizada

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,12 +8,13 @@ export const Login = () => {
     return (
         <div className="min-h-screen flex items-center justify-center">
             {/* Parte izquierda - Formulario */}
+            <div className="absolute top-20 left-20">
+                <Logo />
+            </div>
 
             <div className="flex-1 p-8 flex flex-col justify-center items-center relative">
                 <h1 className="text-4xl font-bold mb-8">Iniciar Sesion</h1>
-                <div className="absolute top-0 left-20">
-                    <Logo />
-                </div>
+
                 <LoginForm />
                 <a href="" className="underline">
                     Ya tienes una cuenta? Inicia sesión
