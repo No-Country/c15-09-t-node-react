14f68cee0db87cf4203fc3c4e401e15ec7a700ea
Jsalvar124 <jsalvar124@gmail.com>
2023-12-06 16:16:07 -0500

    corrección seeder con instancia conn

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,39 @@ const app = express()
 const PORT = process.env.PORT || 5001
 const { conn } = require('./src/db')
 
-app.use(express.json())
+const { Estilos, Maltas } = require('./src/db')
 
+app.use(express.json())
+// TEST ______________________________________________________________________________
+app.get('/', (req, res) => {
+  res.send('Working')
+})
+
+app.get('/styles', async (req, res) => {
+  try {
+    const styles = await Estilos.findAll()
+    if (!styles) {
+      return res.status(404).json({ message: 'not found' })
+    }
+    return res.status(200).json(styles)
+  } catch (error) {
+    console.log({ errorMessage: error.message })
+    return res.status(500).json({ message: error.message })
+  }
+})
+app.get('/malts', async (req, res) => {
+  try {
+    const malts = await Maltas.findAll()
+    if (!malts) {
+      return res.status(404).json({ message: 'not found' })
+    }
+    return res.status(200).json(malts)
+  } catch (error) {
+    console.log({ errorMessage: error.message })
+    return res.status(500).json({ message: error.message })
+  }
+})
+// END TEST ______________________________________________________________________________
 conn.sync({ force: false })
   .then(() => {
     app.listen(PORT, () => {
