fe755e0b4c96e0685e235cd40663ad59211b3c27
CamiloProg <camilom200107@gmail.com>
2023-12-07 09:25:08 -0500

    form

---
+++ frontend/src/components/createBeerComponents/LevadurasSection.jsx
@@ -0,0 +1,42 @@
+export const LevadurasSection = ({ levadura, onChange, onAdd, onRemove }) => {
+  return (
+    <div className="flex flex-col justify-center items-center px-10 py-10">
+      <h2 className="text-2xl pb-4">Levaduras</h2>
+      {levadura.map((item, index) => (
+        <div key={index} className="flex flex-wrap flex-col mb-2">
+          <label className="flex flex-col justify-between mr-4">
+            Nombre:
+            <input
+              className="p-1 border rounded-md mr-2"
+              type="text"
+              name="name"
+              value={item.name}
+              onChange={(e) => onChange(e, index)}
+            />
+          </label>
+
+          <label className="flex flex-col justify-between mr-4">
+            Cantidad:
+            <input
+              className="p-1 border rounded-md mr-2"
+              type="text"
+              name="quantity"
+              value={item.quantity}
+              onChange={(e) => onChange(e, index)}
+            />
+          </label>
+          <button
+            className="p-1 mt-2 bg-red-500 text-white rounded-md"
+            type="button"
+            onClick={() => onRemove(index)}
+          >
+            Eliminar
+          </button>
+        </div>
+      ))}
+      <button type="button" onClick={onAdd} className="p-2 bg-green-500 text-white rounded-md mt-4">
+        Agregar Levadura
+      </button>
+    </div>
+  );
+};
