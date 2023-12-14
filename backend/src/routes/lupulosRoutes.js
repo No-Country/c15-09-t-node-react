b99fdff47a20828521244d61f24f90495d34fb92
unknown <rojasdahianna123@gmail.com>
2023-12-14 17:33:01 -0500

    actualizacion ruta

--- backend/src/routes/lupulosRoutes.js
+++ backend/src/routes/lupulosRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const router = Router()
-const LupulosController = require('../controllers/controllerLupulos')
+const LupulosController = require('../controllers/controllerRecetas')
 
 router.get('/getLupulos', LupulosController.getAllLupulos)
 router.post('/registerLupulos', LupulosController.createLupulos)
