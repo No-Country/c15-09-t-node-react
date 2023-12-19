ea619e392978218943be8aa77c0274f26d56a9c3
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-12-19 14:33:23 -0300

    estilos de Home, llegando del back

--- frontend/src/components/CardIndex.jsx
+++ frontend/src/components/CardIndex.jsx
@@ -1,48 +1,34 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { getAllStyles } from "../services/styles";
+import imagenEstilos from "../assets/images/imageRegister.png";
+
 
 export const CardIndex = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getAllStyles().then((data) => {
+            const dataFiltrada = data.slice(0, 5)
+            setData(dataFiltrada)
+        });
+    }, []);
 
 
-    const estilos = [
-        {
-            imagen: "https://www.elcaminodelacerveza.com/wp-content/uploads/2020/07/Strange-2-650x500.jpg",
-            nombre: "Corona",
-            parrafo: "Cerveza robusta con notas citricas",
-        },
-        {
-            imagen: "https://www.elcaminodelacerveza.com/wp-content/uploads/2020/07/Strange-2-650x500.jpg",
-            nombre: "Ipa",
-            parrafo: "Descripción corta del objeto 1.",
-        },
-        {
-            imagen: "https://www.elcaminodelacerveza.com/wp-content/uploads/2020/07/Strange-2-650x500.jpg",
-            nombre: "Amber",
-            parrafo: "Descripción corta del objeto 1.",
-        },
-        {
-            imagen: "https://www.elcaminodelacerveza.com/wp-content/uploads/2020/07/Strange-2-650x500.jpg",
-            nombre: "Honey",
-            parrafo: "Descripción corta del objeto 1.",
-        },
-        {
-            imagen: "https://www.elcaminodelacerveza.com/wp-content/uploads/2020/07/Strange-2-650x500.jpg",
-            nombre: "Corona",
-            parrafo: "Descripción corta del objeto 1.",
-        },
-    ]
+
     return (
         <div className="flex items-center flex-col mt-16 ">
             <h1 className="font-homemade my-10  text-center"> Estilos de Cervezas</h1>
             <div className="flex gap-x-10 gap-y-5 flex-wrap justify-center">
-                {estilos.map((estilo, index) => (
+                {data.map((estilo, index) => (
                     <div key={index}>
                         {" "}
                         <Link to={"/app/styles"}>
                             <div className="w-48 hover:scale-110 transition-transform rounded overflow-hidden shadow-lg">
-                                <img className="w-full" src={estilo.imagen} alt="Sunset in the mountains" />
+                                <img className="w-full" src={imagenEstilos} alt="Sunset in the mountains" />
                                 <div className="px-6 py-4">
-                                    <div className="font-bold  text-xl mb-2">{estilo.nombre}</div>
-                                    <p className="text-gray-700 text-base">{estilo.parrafo}</p>
+                                    <div className="font-bold  text-xl mb-2">{estilo.name}</div>
+                                    <p className="text-gray-700 text-base">{estilo.category}</p>
                                 </div>
                             </div>{" "}
                         </Link>
--- frontend/src/components/Stylesbeer.jsx
+++ frontend/src/components/Stylesbeer.jsx
@@ -4,80 +4,79 @@ import { getAllStyles } from "../services/styles";
 import imagenEstilos from "../assets/images/imageRegister.png";
 
 export const Stylesbeer = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    getAllStyles().then((data) => setData(data));
-  }, []);
+    useEffect(() => {
+        getAllStyles().then((data) => setData(data));
+    }, []);
 
-  console.log(data);
+    console.log(data);
 
-  const productosPorPagina = 15;
-  const totalPaginas = Math.ceil(data.length / productosPorPagina);
-  const [paginaActual, setPaginaActual] = useState(1);
+    const productosPorPagina = 15;
+    const totalPaginas = Math.ceil(data.length / productosPorPagina);
+    const [paginaActual, setPaginaActual] = useState(1);
 
-  const handlePaginaChange = (nuevaPagina) => {
-    setPaginaActual(nuevaPagina);
-  };
+    const handlePaginaChange = (nuevaPagina) => {
+        setPaginaActual(nuevaPagina);
+    };
 
-  const productosPaginados = data.slice(
-    (paginaActual - 1) * productosPorPagina,
-    paginaActual * productosPorPagina
-  );
+    const productosPaginados = data.slice(
+        (paginaActual - 1) * productosPorPagina,
+        paginaActual * productosPorPagina
+    );
 
-  return (
-    <div className="flex items-center flex-col mt-8 ">
-      <h1 className="font-homemade my-10 self-start">Estilos de cerveza</h1>
-      <div className="flex gap-x-10 gap-y-5 flex-wrap justify-center">
-        {productosPaginados.map((producto, index) => (
-          <div key={index}>
-            {" "}
-            <Link to={`/app/styles/${producto.id}`}>
-              <div className="w-48 hover:scale-110 transition-transform rounded overflow-hidden shadow-lg">
-                <img className="w-full" src={imagenEstilos} alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                  <div className="font-bold font-homemade text-xl mb-2">{producto.name}</div>
-                  <p className="text-gray-700 text-base">{producto.category}</p>
-                </div>
-              </div>{" "}
-            </Link>
-          </div>
-        ))}
-      </div>
+    return (
+        <div className="flex items-center flex-col mt-8 ">
+            <h1 className="font-homemade my-10 self-start">Estilos de cerveza</h1>
+            <div className="flex gap-x-10 gap-y-5 flex-wrap justify-center">
+                {productosPaginados.map((producto, index) => (
+                    <div key={index}>
+                        {" "}
+                        <Link to={`/app/styles/${producto.id}`}>
+                            <div className="w-48 hover:scale-110 transition-transform rounded overflow-hidden shadow-lg">
+                                <img className="w-full" src={imagenEstilos} alt="Sunset in the mountains" />
+                                <div className="px-6 py-4">
+                                    <div className="font-bold font-homemade text-xl mb-2">{producto.name}</div>
+                                    <p className="text-gray-700 text-base">{producto.category}</p>
+                                </div>
+                            </div>{" "}
+                        </Link>
+                    </div>
+                ))}
+            </div>
 
-      {/* Paginación */}
+            {/* Paginación */}
 
-      <div className="flex items-center justify-center mt-20 mb-10 py-2 border-2 border-gray-light  rounded-[26px] ">
-        <button
-          className="mx-1 pl-3 pr-7 cursor-pointer text-gray"
-          onClick={() => handlePaginaChange(paginaActual - 1)}
-          disabled={paginaActual === 1}
-        >
-          &lt;
-        </button>
-        <ul className="flex">
-          {[...Array(totalPaginas).keys()].map((numeroPagina) => (
-            <li
-              key={numeroPagina + 1}
-              className={`mx-1 w-6 h-6 flex cursor-pointer text-center justify-center items-center ${
-                numeroPagina + 1 === paginaActual
-                  ? "  bg-primary rounded-full text-white"
-                  : "text-gray"
-              }`}
-              onClick={() => handlePaginaChange(numeroPagina + 1)}
-            >
-              <span> {numeroPagina + 1} </span>
-            </li>
-          ))}
-        </ul>
-        <button
-          className="mx-1 pr-3 pl-7 cursor-pointer  text-gray"
-          onClick={() => handlePaginaChange(paginaActual + 1)}
-          disabled={paginaActual === totalPaginas}
-        >
-          &gt;
-        </button>
-      </div>
-    </div>
-  );
+            <div className="flex items-center justify-center mt-20 mb-10 py-2 border-2 border-gray-light  rounded-[26px] ">
+                <button
+                    className="mx-1 pl-3 pr-7 cursor-pointer text-gray"
+                    onClick={() => handlePaginaChange(paginaActual - 1)}
+                    disabled={paginaActual === 1}
+                >
+                    &lt;
+                </button>
+                <ul className="flex">
+                    {[...Array(totalPaginas).keys()].map((numeroPagina) => (
+                        <li
+                            key={numeroPagina + 1}
+                            className={`mx-1 w-6 h-6 flex cursor-pointer text-center justify-center items-center ${numeroPagina + 1 === paginaActual
+                                ? "  bg-primary rounded-full text-white"
+                                : "text-gray"
+                                }`}
+                            onClick={() => handlePaginaChange(numeroPagina + 1)}
+                        >
+                            <span> {numeroPagina + 1} </span>
+                        </li>
+                    ))}
+                </ul>
+                <button
+                    className="mx-1 pr-3 pl-7 cursor-pointer  text-gray"
+                    onClick={() => handlePaginaChange(paginaActual + 1)}
+                    disabled={paginaActual === totalPaginas}
+                >
+                    &gt;
+                </button>
+            </div>
+        </div>
+    );
 };
