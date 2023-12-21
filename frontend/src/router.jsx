96d0516ddb61b4fca77b2738834b7d281463a910
redmor1 <lightz17@hotmail.com>
2023-12-21 12:23:57 -0300

    Eliminar unused vars

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Home } from "./pages/Home";
