f95446983ec16c6dcc89fe635e416e0e8ee56ecf
redmor1 <lightz17@hotmail.com>
2023-12-22 18:06:42 -0300

    Otro bugfix de user.js y RequireAuth.jsx

--- frontend/src/pages/Auth/RequireAuth.jsx
+++ frontend/src/pages/Auth/RequireAuth.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getUserByToken } from "../../services/user";
 import { setUser } from "../../redux/actions/userActions";
 
 function RequireAuth({ children }) {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const [loading, setLoading] = useState(true);
@@ -13,8 +14,6 @@ function RequireAuth({ children }) {
   useEffect(() => {
     // Obtén el token del almacenamiento local
     const authToken = localStorage.getItem("authToken");
-    console.log(authToken);
-
     if (authToken) {
       // Si el token existe, haz una solicitud al servidor para obtener los datos del usuario
       getUserByToken(authToken)
@@ -22,10 +21,11 @@ function RequireAuth({ children }) {
           // Si la solicitud tiene éxito, pon los datos del usuario en el estado de Redux
           dispatch(setUser(user));
           localStorage.setItem("authToken", authToken);
-          console.log("este es el user redux", user);
         })
         .catch((error) => {
-          console.error(error);
+          localStorage.removeItem("authToken");
+          console.error("Error: ", error);
+          navigate("/");
         })
         .finally(() => {
           // Independientemente de si la solicitud tiene éxito o falla, establece 'loading' en 'false'
@@ -36,23 +36,17 @@ function RequireAuth({ children }) {
       localStorage.removeItem("authToken");
       setLoading(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
 
   // Si 'user' o 'user.token' es 'undefined', redirige al usuario
   if (loading) {
-    console.log("esto es dentro del loading");
-
     return null; // o un componente de carga
   }
 
   if (user && localStorage.getItem("authToken")) {
-    console.log("esto es user.token", user.token);
     return children;
   } else {
-    // console.log("esto es user, dentro del condicional", user);
-    // console.log("esto es user.token, dentro del condicional", user.token);
-    // console.log(user && user.token);
-
     return <Navigate to="/" />;
   }
 }
